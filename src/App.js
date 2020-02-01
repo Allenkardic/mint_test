8bf5bab8debc0e9a72bcfd709d1d6b3f6de31882
Aliakwe <aliakwe@yahoo.com>
2020-02-01 19:55:58 +0100

    implemented the navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@
 import React from "react";
 import "./App.css";
 
+import Navbar from "./components/bars/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>hello</h1>
     </div>
   );
---
+++ src/components/bars/Navbar.js
@@ -0,0 +1,46 @@
+/** @format */
+
+import React, { Component } from "react";
+import "../../css/Navbar.css";
+import IMG from "../../images/img1.jpg";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="navbar-items">
+          <h2>TransMonitor</h2>
+        </div>
+
+        <div className="navbar-items">
+          <span>
+            <i class="fas fa-search" />
+          </span>
+          <input placeholder="Search..." />
+        </div>
+
+        <div className="navbar-items">
+          <div className="navbar-items-contents">support</div>
+          <div className="navbar-items-contents">FAQ</div>
+          <div className="navbar-items-contents">
+            <i class="far fa-bell bell" />
+            <span className="bell-number">2</span>
+          </div>
+          <div>
+            <div
+              style={{ fontSize: "0.5em" }}
+              className="navbar-items-contents">
+              Hello
+            </div>
+            <div className="navbar-items-contents">Olarenwanju</div>
+          </div>
+          <div>
+            <img className="img" src={IMG} alt="profile" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
